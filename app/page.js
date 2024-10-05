063936faab76ba543846e9a558679a5556613e93
Adam <adamaizal4477@gmail.com>
2024-10-06 02:41:33 +1300

    Basic LatestBlogs page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import BlogItem from "@/Components/BlogItem";
 import Header from "@/Components/Header.jsx";
 
 export default function Home() {
   return (
     <>
     <Header></Header>
+    <BlogItem></BlogItem>
     </>
   );
 }
